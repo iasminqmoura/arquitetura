08d69443ca092ff6b8c8f132937f96ed2b2ea160
iasminqmoura <36743683+iasminqmoura@users.noreply.github.com>
2019-04-02 11:13:57 -0300

    Add files via upload

---
+++ Trabalho - Circuitos Digitais AND.asm
@@ -0,0 +1,40 @@
+
+#include <p18f4550.inc>
+
+	CONFIG WDT=OFF; disable watchdog timer
+	CONFIG MCLRE = ON; MCLEAR Pin on
+	CONFIG DEBUG = ON; Enable Debug Mode
+	CONFIG LVP = OFF; Low-Voltage programming disabled (necessary for debugging)
+	CONFIG FOSC = INTOSCIO_EC;Internal oscillator, port function on RA6
+
+    org 0; start code at 0
+
+
+
+CODE
+
+Start:
+    MOVLW 0xEF
+    MOVWF TRISD
+    
+MainLoop:
+    
+    
+    ;Copia o PORTD para a mem 0
+    MOVFF PORTD, 0x00
+    
+    MOVF 0x00, W
+    
+    RLNCF WREG
+    
+    ANDWF 0x00, W
+    
+    RLNCF WREG
+    RLNCF WREG
+    RLNCF WREG
+    
+    MOVWF PORTD
+    
+    GOTO MainLoop
+
+	END
